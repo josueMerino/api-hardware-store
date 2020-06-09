7fe074962eb7c7d1bd791662624c6bd915c2a124
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-06-09 13:09:00 +0200

    Fixing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,6 @@ class Kernel extends HttpKernel
             EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
 
         ],
     ];
