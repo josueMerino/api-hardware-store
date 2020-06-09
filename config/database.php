2fee472473c503fe5badbae5cf2d837d63611eeb
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-06-09 11:25:09 +0200

    change db to postgres

--- config/database.php
+++ config/database.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://rjbibapphkcscs:4a885f08f5d27bb7e03dd2fbad7904957d455ea88c7c8b074e5aff0c5bd4d83a@ec2-34-230-231-71.compute-1.amazonaws.com:5432/d1bpj72a8q1a6d');
+
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +68,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
