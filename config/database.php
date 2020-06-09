6f570e8fba90c115f188269fe06e36188ac395ba
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-06-09 11:34:58 +0200

    Fixing

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'password' => $DATABASE_URL["password"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
